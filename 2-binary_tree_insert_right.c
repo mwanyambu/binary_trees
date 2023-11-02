8068d77098540591cb90b4d1f85ee36e1892b9cc
mwanyambu <jmwanyambu@gmail.com>
2023-11-02 14:59:07 -0700

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,12 +8,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	new_node = binary_tree_node(parent, value);
 
 	if (parent == NULL)
 	{
