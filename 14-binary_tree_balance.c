d820363f4dfe45c43c87a3a96535d24243fe6eba
mwanyambu <jmwanyambu@gmail.com>
2023-11-03 08:47:36 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,11 +7,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t height_left, height_right;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right);
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	return (height_left - height_right);
 }
 /**
  * binary_tree_height - gets the height of binary tree
@@ -20,12 +26,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left, height_right;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t height_left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-	size_t height_right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
 	return ((height_left > height_right) ? height_left : height_right);
 }
