bcdb9cc69d12796306b34d8e65be8a05b3a48d54
mwanyambu <jmwanyambu@gmail.com>
2023-11-03 08:28:14 -0700

    binary height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	size_t height_left = 0, height_right = 0;
 
-	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	if (tree->left)
+	{
+		height_left += 1 + binary_tree_height(tree->left);
+	}
 
-	return (height_left > height_right ? height_left : height_right);
+	if (tree->right)
+	{
+		height_right += 1 + binary_tree_height(tree->right);
+	}
+
+	if (height_left > height_right)
+	{
+		return (height_left);
+	}
+	else
+	{
+		return (height_right);
+	}
 }
